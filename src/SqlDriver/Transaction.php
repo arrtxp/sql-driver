ae9edc2575cdb7395ce5cbe57e53ba251884a513
Tomasz Dudzinski <t92dudzinski@gmail.com>
2025-05-03 01:21:20 +0200

    Transaction

---
+++ src/SqlDriver/Transaction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SqlDriver;
+
+use SqlDriver\Adapter;
+
+final class Transaction
+{
+    protected bool $transaction = false;
+
+    public function __construct(protected readonly Adapter $adapter)
+    {
+    }
+
+    public function begin(): void
+    {
+        $this->adapter->pdo()->beginTransaction();
+        $this->transaction = true;
+    }
+
+    public function commit(): void
+    {
+        $this->adapter->pdo()->commit();
+        $this->transaction = false;
+    }
+
+    public function rollback(): void
+    {
+        $this->adapter->pdo()->rollBack();
+        $this->transaction = false;
+    }
+
+    public function isTransaction(): bool
+    {
+        return $this->transaction;
+    }
+}
